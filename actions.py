1318312352d9e1ef51fc648519726b1e4f3ecfa2
Meshal Al-zahrni <sh3lool@hotmail.com>
2019-02-06 09:03:14 +0300

    project two done

--- actions.py
+++ actions.py
@@ -2,41 +2,76 @@
 from data import stores
 from components import Cart
 
-site_name = ""  # Give your site a name
+site_name = "HFX"  # Give your site a name
 
 def welcome():
-    print("Welcome to %s\nFeel free to shop throughout the stores we have, and only checkout once!" % site_name)
+	print("Welcome to %s\nFeel free to shop throughout the stores we have, and only checkout once!" % site_name)
 
 def print_stores():
-    """
-    prints the list of stores in a nice readable format.
-    """
-    # your code goes here!
+	"""
+	prints the list of stores in a nice readable format.
+	"""
+	# your code goes here!
+	for store in stores:
+		print (store.name)
 
 def get_store(store_name):
-    """
-    receives a name for a store, and returns the store object with that name.
-    """
-    # your code goes here!
+	"""
+	receives a name for a store, and returns the store object with that name.
+	"""
+	# your code goes here!
+	for store in stores:
+		if store.name == store_name:
+			return store
+	return False
 
 def pick_store():
-    """
-    prints list of stores and prompts user to pick a store.
-    """
-    # your code goes here!
+	"""
+	prints list of stores and prompts user to pick a store.
+	"""
+	# your code goes here!
+	print_stores()
+	my_store = input("Which store would you like? ('checkout to leave')")
+	while my_store.lower() != "checkout":
+		if get_store(my_store):
+		   return get_store(my_store)
+		else:
+			my_store= input ("Invalid! Please try again. . .")
+	return "checkout"
 
+		
+
+				
 def pick_products(cart, picked_store):
-    """
-    prints list of products and prompts user to add products to card.
-    """
-    # your code goes here!
+	"""
+	prints list of products and prompts user to add products to card.
+	"""
+	# your code goes here!
+	picked_store.print_products()
+	my_product = input("What would you like to add to the car?")
+	while my_product.lower() != 'back':
+		for product in picked_store.products:
+			if product.name == my_product:
+			   cart.add_to_cart(product)
+		my_product = input("What wlse would you like to add?")
+
+		
+
+
 
 def shop():
-    """
-    The main shopping functionality
-    """
-    cart = Cart()
-    # your code goes here!
+	"""
+	The main shopping functionality
+	"""
+	cart = Cart()
+	# your code goes here!
+
+	store = pick_store()
+	while store != "checkout":
+		pick_products(cart, store)
+		store = pick_store()
+	cart.checkout()				
+		
 
 def thank_you():
-    print("Thank you for shopping with us at %s" % site_name)
+	print("Thank you for shopping with us at %s" % site_name)
--- components.py
+++ components.py
@@ -1,62 +1,92 @@
 # CLASSES AND METHODS
 class Store():
-    def __init__(self, name):
-        """
-        Initializes a new store with a name.
-        """
-        # your code goes here!
+	def __init__(self, name):
+		"""
+		Initializes a new store with a name.
+		"""
+		# your code goes here!
+		self.products = []
+		self.name = name
 
-    def add_product(self, product):
-        """
-        Adds a product to the list of products in this store.
-        """
-        # your code goes here!
 
-    def print_products(self):
-        """
-        Prints all the products of this store in a nice readable format.
-        """
-        # your code goes here!
+
+	def add_product(self, product):
+		"""
+		Adds a product to the list of products in this store.
+		"""
+		# your code goes here!
+		self.products.append(product)
+
+	def print_products(self):
+		"""
+		Prints all the products of this store in a nice readable format.
+		"""
+		# your code goes here!
+		for item in self.products:
+			print(item)
 
 
 class Product():
-    def __init__(self, name, description, price):
-        """
-        Initializes a new product with a name, a description, and a price.
-        """
-        # your code goes here!
+	def __init__(self, name, description, price):
+		"""
+		Initializes a new product with a name, a description, and a price.
+		"""
+		# your code goes here!
+		self.name = name    
+		self.description = description
+		self.price = price
 
-    def __str__(self):
-        # your code goes here!
+	def __str__(self):
+		# your code goes here!
+		return("(%s, %s, %s)" % (self.name, self.description, self.price))
+
 
 
 class Cart():
-    def __init__(self):
-        """
-        Initializes a new cart with an empty list of products.
-        """
-        # your code goes here!
+	def __init__(self):
+		"""
+		Initializes a new cart with an empty list of products.
+		"""
+		# your code goes here
+		self.cart = []
 
-    def add_to_cart(self, product):
-        """
-        Adds a product to this cart.
-        """
-        # your code goes here!
+	def add_to_cart(self, product):
+		"""
+		Adds a product to this cart.
+		"""
+		# your code goes here!
+		self.cart.append(product)
 
-    def get_total_price(self):
-        """
-        Returns the total price of all the products in this cart.
-        """
-        # your code goes here!
+	def get_total_price(self):
+		"""
+		Returns the total price of all the products in this cart.
+		"""
+		# your code goes here!
+		total=0
+		for product in self.cart:
+			total+=product.price
+		return total    
 
-    def print_receipt(self):
-        """
-        Prints the receipt in a nice readable format.
-        """
-        # your code goes here!
+	def print_receipt(self):
+		"""
+		Prints the receipt in a nice readable format.
+		"""
+		# your code goes here!
+		for product in self.cart:
+			print( product )
+		(print ("Your Total Is %s" % self.get_total_price() ))
 
-    def checkout(self):
-        """
-        Does the checkout.
-        """
-        # your code goes here!
+	def checkout(self):
+		"""
+		Does the checkout.
+		"""
+		# your code goes here!
+		self.print_receipt()
+		user_input= input (" Do you confirm (y/n)? ").lower()
+		if user_input == "y":
+			print (" Your order is confirm")
+		elif user_input=="n":
+			ptint (" your oder is cancel")
+		else:
+			print("wrong input")
+			self.checkout()
